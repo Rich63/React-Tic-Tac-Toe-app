32cb6006b59a63e57e4f6984dc416472207263a0
Rich63 <rich63-develop@outlook.com>
2019-11-22 16:29:01 +0100

    Untill Completing the game - Taking turns

--- src/index.js
+++ src/index.js
@@ -3,27 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // Change Square’s render method to show that value by replacing {/* TODO */} with {this.props.value}:
-class Square extends React.Component {
-    render() {
-      return (
-        <button
-          className="square"
-          onClick={ () => alert(`Clicked on ${ this.props.value }`) }
-        >
-          { this.props.value }
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={ props.onClick }
+    >
+      { props.value }
+    </button>
+  );
+}
   
   // In Board’s renderSquare method, change the code to pass a prop called value to the Square:
   class Board extends React.Component {
+    // Note: In JavaScript classes, you need to always call super when defining 
+    // the constructor of a subclass. All React component classes that have a 
+    // constructor should start it with a super(props) call.
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      }
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({ 
+        squares,
+        xIsNext: !this.state.xIsNext,
+      })
+    }
+
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square
+          value={ this.state.squares[i] } 
+          onClick={ () => this.handleClick(i) }
+        />
+      );
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
